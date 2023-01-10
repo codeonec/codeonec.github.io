0d8134f34e7173aa95fdfabbae3c64717236dc11
Ritul Patel <ritul.patel904@gmail.com>
2023-01-10 15:24:40 -0500

    Added work, updated navbar

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
+let mouseCursor = document.querySelector(".cursor");
+var link = document.querySelectorAll("a");
 
-
-let mouseCursor = document.querySelector('.cursor');
-    var link = document.querySelectorAll('a');
-
-    const cursor = (e) => {
-    // mouseCursor.style.top = e.clientY + 'px';
-    // mouseCursor.style.left = e.clientX + 'px';
-    mouseCursor.style.transform =   'translate(-50%,-50%)'+'translate('+e.clientX+'px'+','+e.clientY + 'px)';
-}
-window.addEventListener('mousemove',cursor);
-link.forEach(element => {
-    element.addEventListener("mouseover", ()=>{
-        mouseCursor.classList.add('link-grow');
-    });
-    element.addEventListener("mouseleave", ()=>{
-        mouseCursor.classList.remove('link-grow');
-    });
+const cursor = (e) => {
+  // mouseCursor.style.top = e.clientY + 'px';
+  // mouseCursor.style.left = e.clientX + 'px';
+  mouseCursor.style.transform =
+    "translate(-50%,-50%)" +
+    "translate(" +
+    e.clientX +
+    "px" +
+    "," +
+    e.clientY +
+    "px)";
+};
+window.addEventListener("mousemove", cursor);
+link.forEach((element) => {
+  element.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+  });
+  element.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+  });
 });
-
-
 
 const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".scroll-container"),
-    smooth: true
+  el: document.querySelector(".scroll-container"),
+  smooth: true,
 });
+
+document.querySelector(".about-link").onclick = () => {
+  locoScroll.scrollTo("#about");
+};
+document.querySelector(".work-link").onclick = () => {
+  locoScroll.scrollTo("#work");
+};
+document.querySelector(".contact-link").onclick = () => {
+  locoScroll.scrollTo("#contact");
+};
